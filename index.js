601bd8bfc5ef45d83903ef64757fb6b6ba7a1118
Chris <1527164+aprea@users.noreply.github.com>
2025-01-13 00:21:57 +1100

    Remove random

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ class StockAvailabilityFinder {
                                     stockData[i].storeData.push({
                                         id: store.Id,
                                         name: store.Name,
-                                        itemInStock: Math.random() > 0.5 ? true : store.SearchedProductIsInStock,
+                                        itemInStock: store.SearchedProductIsInStock,
                                     }); 
                                 }
 
